6546d860f189a44b97fc87ff2cc484358cb10bd0
bati11 <mail.bati11@gmail.com>
2013-10-01 22:36:29 +0900

    tomcatのバージョンを6から7へ変更

--- site-cookbooks/tomcat/recipes/default.rb
+++ site-cookbooks/tomcat/recipes/default.rb
@@ -6,12 +6,29 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-package "tomcat6" do
+package "yum-priorities" do
+  action :install
+end
+
+bash "download JPackage" do
+  code <<-EOC
+    wget http://mirrors.dotsrc.org/jpackage/6.0/generic/free/RPMS/jpackage-release-6-3.jpp6.noarch.rpm -P /tmp/
+  EOC
+  creates "/tmp/jpackage-release-6-3.jpp6.noarch.rpm"
+end
+
+package "jpackage" do
+  action :install
+  source "/tmp/jpackage-release-6-3.jpp6.noarch.rpm"
+  provider Chef::Provider::Package::Rpm
+end
+
+package "tomcat7" do
   action :upgrade
   #version "6.0.24-57.el6_4"
 end
 
-service "tomcat6" do
+service "tomcat7" do
   action [ :enable, :start]
   supports :status => true, :restart => true, :reload => true, :version => true
 end
