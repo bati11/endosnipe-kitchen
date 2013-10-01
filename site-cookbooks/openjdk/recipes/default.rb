9d1f33bcbdbb838ed06c3915bc88662c47424170
bati11 <mail.bati11@gmail.com>
2013-10-01 22:35:39 +0900

    Java7のレシピを追加

---
+++ site-cookbooks/openjdk/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: openjdk
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "java-1.7.0-openjdk" do
+  action :install
+end
